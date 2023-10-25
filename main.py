06700ac6b96c1c52f1a9e168606c301a8c7de2b7
Jason Ang <jason8417@icloud.com>
2023-10-25 15:36:05 -0400

    Jason A option 1 encode password

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('adf')
+def main():
+    menu_option = None
+    password = ""
+    encoded_password = ""
+    while menu_option != '3':
+        menu_option = input("Please enter an option: ")
+        # Jason edit: encode password (add 3 to each int in string)
+        if menu_option == '1':
+            password = input("Please enter your password to encode: ")
+            encoded_password = ""
+            for num in password:
+                encoded_password += str(int(num) + 3)
+            print("Your password has been encoded and stored!")
+        elif menu_option == '2':
+            pass
+            # print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
+
+if __name__ == '__main__':
+    main()
